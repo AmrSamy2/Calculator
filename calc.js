9650c19e6b4a55567515158eb0c9fc4c40982f57
Amr Samy <130178408+AmrSamy2@users.noreply.github.com>
2023-05-24 14:38:00 +0300

    changed '/' to '÷"

--- calc.js
+++ calc.js
@@ -42,7 +42,7 @@ class Calculator {
       case '*':
         computation = prev * current
         break
-      case '/':
+      case '÷':
         computation = prev / current
         break
       default:
